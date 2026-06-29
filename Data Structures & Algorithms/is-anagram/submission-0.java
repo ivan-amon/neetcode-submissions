c6c6991c815911c8ca1b8fb9dd38ec735160a354
Iván Amón Bystrova <156613208+ivan-amon@users.noreply.github.com>
2026-06-29 12:57:31 +0200

    Bulk sync: 7 submissions

---
+++ Data Structures & Algorithms/is-anagram/submission-0.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if(s.length() != t.length()) {
+            return false;
+        }
+
+        int[] chars = new int[26];
+
+        for(int i = 0; i < s.length(); i++) {
+            chars[s.charAt(i) - 'a'] += 1;
+        }
+
+        for(int i = 0; i < t.length(); i++) {
+            chars[t.charAt(i) - 'a'] -= 1;
+        }
+
+        for(int i = 0; i < chars.length; i++) {
+            if(chars[i] != 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
